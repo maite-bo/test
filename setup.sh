fc2e16c24eec9ff7dad43c5c93d2b5ab383f74a8
Marie <mariethereseodette.biagui@edu.univ-paris13.fr>
2023-09-13 23:56:24 +0200

    modification setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "https://sof-test-1c6c58d57243.herokuapp.com"n
+echo "\
+[general]\n\
+email = "https://sof-test-1c6c58d57243.herokuapp.com"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
